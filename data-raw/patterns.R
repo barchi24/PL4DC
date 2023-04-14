089b7f056f2f02d0d5d57a07450b7676c6b0f670
barchi24 <gregorio.barchino@alu.uclm.es>
2023-04-14 11:11:57 +0200

    Modificación expresión teléfono

--- data-raw/patterns.R
+++ data-raw/patterns.R
@@ -4,7 +4,7 @@ name <- c("Email",
           "IP address")
 
 pattern <- c("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-             "^(\\+\\d{1,3}\\s?)?(\\d{9}|\\d{3}((\\s\\d{2}){3}|(\\s\\d{3}){2}))$",
+             "^[+][0-9]{2}[0-9]{9}$",
              "^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$",
              "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$")
 
@@ -18,3 +18,6 @@ patterns <- data.frame(Name = name,
                        Example = example)
 
 usethis::use_data(patterns, overwrite = TRUE)
+
+# Teléfonos válidos (no solo +34610587480):
+# "^(\\+\\d{1,3}\\s?)?(\\d{9}|\\d{3}((\\s\\d{2}){3}|(\\s\\d{3}){2}))$"
