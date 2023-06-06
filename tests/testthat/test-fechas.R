8364d1e8e99b8d0f3525b7f3ad92e116364fe755
Gregorio Barchino <gregorio.barchino@alu.uclm.es>
2023-06-06 10:38:17 +0200

    Eliminación de los tests

--- tests/testthat/test-fechas.R
+++
@@ -1,9 +0,0 @@
-test_that("Test standardize_dates function with valid input", {
-  dates <- c("12/31/2020", "01/01/2021")
-  expect_equal(standardize_dates(dates, input_format = c("mdy"), output_format = "%Y-%m-%d"), c("2020-12-31", "2021-01-01"))
-})
-
-test_that("Test standardize_dates function with invalid input format", {
-  dates <- c("12/31/2020", "01/01/2021")
-  expect_warning(standardize_dates(dates, input_format = c("myd"), output_format = "%Y-%m-%d"), "No formats found")
-})
